bc08a70e398f40bddcd952f909b0e769a16f7748
ALVLP <alvlp.fun@gmail.com>
2024-11-21 07:20:08 +0700

    Create SetWallpaper.ps1

---
+++ SetWallpaper.ps1
@@ -0,0 +1,20 @@
+# Define the wallpaper path
+$wallpaperPath = "C:\Wallpapers\MyWallpaper.jpg"
+
+# Set the wallpaper
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+    public const int SPI_SETDESKWALLPAPER = 20;
+    public const int SPIF_UPDATEINIFILE = 0x01;
+    public const int SPIF_SENDCHANGE = 0x02;
+    public static void Set(string path) {
+        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+    }
+}
+"@
+[Wallpaper]::Set($wallpaperPath
+)
