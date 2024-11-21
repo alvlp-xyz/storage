38303f3606018b6a496b6963adfe30bf11a2a92e
ALVLP <alvlp.fun@gmail.com>
2024-11-21 07:41:37 +0700

    Update SetWallpaper.ps1

--- SetWallpaper.ps1
+++ SetWallpaper.ps1
@@ -1,20 +1,25 @@
 # Define the wallpaper path
 $wallpaperPath = "C:\Wallpapers\MyWallpaper.jpg"
 
-# Set the wallpaper
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class Wallpaper {
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-    public const int SPI_SETDESKWALLPAPER = 20;
-    public const int SPIF_UPDATEINIFILE = 0x01;
-    public const int SPIF_SENDCHANGE = 0x02;
-    public static void Set(string path) {
-        SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+while ($true) {
+    # Set the wallpaper
+    Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_UPDATEINIFILE = 0x01;
+        public const int SPIF_SENDCHANGE = 0x02;
+        public static void Set(string path) {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
     }
+    "@
+    
+    [Wallpaper]::Set($wallpaperPath)
+
+    # Wait for 1 second before resetting the wallpaper again
+    Start-Sleep -Seconds 1
 }
-"@
-[Wallpaper]::Set($wallpaperPath
-)
